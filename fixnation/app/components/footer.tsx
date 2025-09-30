d49ff11fe8024b4e13bbc554d4dee2ad44f10a4c
MementoM0RI <Braskerud08@gmail.com>
2025-09-30 08:59:32 +0200

    it actually works?!?!?!?
    I ONLY HAD TO PUT THEM INTO FOLDERS!!!! IM GONNA FUCKING KILL SOMEONE!!!!

--- fixnation/app/components/footer.tsx
+++ fixnation/app/components/footer.tsx
@@ -29,7 +29,7 @@ const BottomBar: React.FC = () => (
             }}
         >
             <div style={{ display: 'flex', gap: '16px' }}>
-                <Link href="fixnation/app/aboutus" style={{ color: '#FFFFFF', textDecoration: 'none' }}>
+                <Link href="/aboutus" style={{ color: '#FFFFFF', textDecoration: 'none' }}>
                     ABOUT US
                 </Link>
                 <Link href="fixnation/app/contactus" style={{ color: '#FFFFFF', textDecoration: 'none' }}>
